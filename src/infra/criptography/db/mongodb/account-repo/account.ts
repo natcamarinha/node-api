6db69915170d87bc1aad4ea3fb9c0d6c938ddb7b
Natalia Camarinha <natcamarinha@gmail.com>
2023-01-06 16:41:43 -0300

    feat: ensure AccountMongoRepo returns an account on success

---
+++ src/infra/criptography/db/mongodb/account-repo/account.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from "../../../../../data/protocols/addAccountRepository";
+import { AccountModel } from "../../../../../domain/models/account";
+import { AddAccountModel } from "../../../../../domain/usecases/add-account";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class AccountMongoRepo implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)   
+    const accountId = result.insertedId.toString()
+    return MongoHelper.map(accountData, accountId)
+  }
+}
---
+++ src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,26 @@
+import { Collection, MongoClient } from "mongodb"
+import { AccountModel } from "../../../../../domain/models/account"
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(url)
+  },
+
+  async disconnect(): Promise<void> {
+    this.client.close()
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    return this.client.db().collection(name)
+  },
+
+  map (accountData: any, accountId: string): AccountModel {
+    return {
+      email: accountData.email,
+      id: accountId,
+      name: accountData.name,
+      password: accountData.password
+    }
+  }
+}
